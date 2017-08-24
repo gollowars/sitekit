f601a5936f570d747f5b1f8cc40e5e8d87c2635c
Yoshizumi Ashikawa <yoshizumi.ashikawa@gmail.com>
2017-08-24 11:45:35 +0900

    change webpack babel settings

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require("webpack")
 module.exports = {
   watch: false,
   entry: {
-    bundle: ["babel-polyfill", "./src/js/main.js"]
+    bundle: ["./src/js/main.js"]
     // sp__bundle: "./src/js/sp__main.js",
     // detectpc: "./src/js/detectpc.js",
     // detectsp: "./src/js/detectsp.js"
@@ -16,16 +16,64 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    loaders:[
-      {test: /\.js$/, exclude: /node_modules|modules/, loaders: ['babel-loader', 'eslint-loader']},
-      {test: /\.(glsl|vs|fs)$/, loader: 'webpack-glsl-loader'},
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader:  'babel-loader',
+          options: {
+            presets: [
+              'env',
+              'stage-3',
+              'es2017'
+            ],
+            plugins: [
+              'transform-async-to-generator',
+              ["transform-class-properties", { "spec": true }],
+              'babel-plugin-espower',
+              ['transform-runtime', {
+                regenerator: true,
+                polyfill: true,
+                helper: true,
+              }],
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(glsl|vs|fs)$/,
+        use: 'webpack-glsl-loader'
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /(animation\.gsap|SplitText)\.js$/,
+        use: "imports-loader?define=>false"
+      },
+      {
+        test: /\.(vert|frag|glsl)$/,
+        use:[
+          'raw-loader',
+          'glslify-loader'
+        ]
+      },
     ]
   },
   plugins: [
+    // new webpack.ProvidePlugin({
+    //   'GyroNorm':path.join(__dirname, './node_modules/gyronorm/dist/gyronorm.complete.min.js'),
+    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      minChunks: Infinity,
+      name: 'bundle'
+    }),
     new webpack.DefinePlugin({
       WEBPACK_DEFINITION: JSON.stringify(env.config),
       BUILD_VERSION: JSON.stringify(env.VERSION)
     })
   ],
-  devtool: '#inline-source-map'
+  devtool: 'source-map',
 }
