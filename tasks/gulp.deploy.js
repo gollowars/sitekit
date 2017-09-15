02f429f514cf087b7b33d7d96e6009116f566cc8
Yoshizumi Ashikawa <yoshizumi.ashikawa@gmail.com>
2017-09-15 14:41:32 +0900

    add s3 upload

--- tasks/gulp.deploy.js
+++ tasks/gulp.deploy.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import gulpLoadPlugins from 'gulp-load-plugins'
 const $ = gulpLoadPlugins({pattern: ['gulp-*', 'gulp.*', '@*/gulp{-,.}*']})
 import path from 'path'
+import s3 from 's3'
 
 let config = require(path.join(__dirname,"../deploy.json"))
 const distRoot = path.join(__dirname,"../public/")
@@ -23,4 +24,45 @@ gulp.task('deploy_dev', () => {
         'node_modules'
       ]
     }))
+})
+
+
+gulp.task('s3-sync',() => {
+  let client = s3.createClient({
+    maxAsyncS3: 20,     // this is the default 
+    s3RetryCount: 3,    // this is the default 
+    s3RetryDelay: 1000, // this is the default 
+    multipartUploadThreshold: 20971520, // this is the default (20 MB) 
+    multipartUploadSize: 15728640, // this is the default (15 MB) 
+    s3Options: {
+      accessKeyId: config.s3.dst.id,
+      secretAccessKey: config.s3.dst.secret,
+      endpoint: "s3-ap-northeast-1.amazonaws.com"
+      // any other options are passed to new AWS.S3() 
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+    },
+  })
+  console.log('client:',client)
+  // upload
+  let params = {
+    localFile: path.join(__dirname,"../public/assets/images/profile.png"),
+    s3Params: {
+      Bucket: config.s3.dst.bucket,
+      Key: "profile.png"
+    }
+  }
+
+  let uploader = client.uploadFile(params)
+  uploader.on('error', function(err) {
+    console.error("unable to upload:", err.stack)
+  })
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressMd5Amount,
+              uploader.progressAmount, uploader.progressTotal)
+  })
+  uploader.on('end', function() {
+    console.log("done uploading")
+  })
+
+
 })
